b0ced25813e31acb88d88db707dadb59861c1a4c
Xuan Tho <tholxph31196@fpt.edu.vn>
2024-07-15 12:40:59 +0700

    initial commmit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="w-[25%] h-full p-w flex-col gap-2 text-white hidden lg:flex">
+      <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around "></div>
+    </div>
+  );
+};
+
+export default Sidebar;
