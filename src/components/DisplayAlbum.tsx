6632d43f8a229bd355562d38446889abbd98a219
Xuan Tho <tholxph31196@fpt.edu.vn>
2024-07-15 18:12:46 +0700

    feature[audioControl]

--- src/components/DisplayAlbum.tsx
+++ src/components/DisplayAlbum.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./Navbar";
 import { useNavigate, useParams } from "react-router-dom";
 import {
@@ -6,10 +6,12 @@ import {
   assets,
   songsData,
 } from "../assets/frontend-assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 const DisplayAlbum = () => {
   const { id } = useParams();
   const albumData = albumsData[id];
+  const { playWithId } = useContext(PlayerContext);
   return (
     <>
       <Navbar />
@@ -45,8 +47,9 @@ const DisplayAlbum = () => {
       <hr />
       {songsData.map((item, idx) => (
         <div
+          onClick={() => playWithId(item.id)}
           key={idx}
-          className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b]
+          className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer
         "
         >
           <p className="text-white">
