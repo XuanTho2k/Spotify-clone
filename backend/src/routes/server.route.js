f8465cbeadc372362292db4c70e17f16b6e2cd06
Xuan Tho <tholxph31196@fpt.edu.vn>
2024-07-17 17:31:55 +0700

    fix[deploy]

--- backend/src/routes/server.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import songRouter from "./song.route.js";
-import albumRouter from "./album.route.js";
-const routerServer = express.Router();
-
-routerServer.use("/songs", songRouter);
-routerServer.use("/albums", albumRouter);
-
-export default routerServer;
